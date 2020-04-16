35d36800d7bf85b8d6e0456b53b27c9ea2e2805f
Ronny Toribio <rtoribio@transparent.com>
2020-04-16 16:28:37 -0400

    fixed some issues

--- postV2.py
+++ postV2.py
@@ -21,32 +21,28 @@ class User:
     def uuid(self,jsession):
         """ will bring the correct UUID for the user """ 
 
-        # payload = {'login':config.username, 'password':config.password}
-        # login_url = 'https://tlodockertest.transparent.local:7070/rest/admin/login'
+        payload = {'login':config.username, 'password':config.password}
+        login_url = 'https://tlodockertest.transparent.local:7070/rest/admin/login'
         profile_search = f'https://tlodockertest.transparent.local:7070/rest/user/profile/{self.account_name}'
 
         search_request = requests.get(profile_search,headers={'Cookie':f'JSESSIONID={jsession}'}, verify=False)
         uuid = [search_request.json()[lst].get('userUuid') for lst in range(len(search_request.json())) if search_request.json()[lst].get('nodeName') == self.node_name][0]
-        # print(uuid)
+        return uuid
     
 
 class Notification:
-    def __init__(self):
-        pass        
-    # def jsessionid(self):
-    #     """ uses the stick-e url to log to get the jsession id """
-    #     payload = {'login':self.username, 'password':self.password}
-    #     login_url = 'https://tlodockertest.transparent.local:7070/rest/admin/login'
-    #     jsessionid = requests.Session().post(login_url, json=payload, verify=False).cookies.values()[0]
-    #     return jsessionid
+    def __init__(self, UUID, jsessionid):
+        self.UUID = UUID
+        self.jsessionid = jsessionid
+    
         
 
 
-    def send_daily(self, device_type):
+    def send_daily(self,device_type, k_lang, l_lang):
         """ Posts the requests for daily refreshser """
 
-        sticke_test_url = f'https://192.168.254.54:7070/rest/external/{config.userUid}/REFRESHER/{device_type}/{config.knownLanguage}/{config.learnedLanguage}'
-        R = requests.post(sticke_test_url, headers={'Host':'notifications.transparent.com','Cookie':f'JSESSIONID={Tester.jsessionid()}'}, verify=False)
+        sticke_test_url = f'https://192.168.254.54:7070/rest/external/{self.UUID}/REFRESHER/{device_type}/{k_lang}/{l_lang}'
+        R = requests.post(sticke_test_url, headers={'Host':'notifications.transparent.com','Cookie':f'JSESSIONID={self.jsessionid}'}, verify=False)
 
         print(R.status_code)
         print(R.text)
--- test.py
+++ test.py
@@ -1,7 +1,21 @@
-from postV2 import Tester, User
+from postV2 import Tester, User, Notification
 
 
 ronny = Tester('rtoribio','Ac44life#')
 
 sponge_bob = User('sponge_bob','ronny_node')
 
+# uuid = sponge_bob.uuid(ronny.jsessionid())
+
+# print(uuid)
+
+# push = Notification(uuid, ronny.jsessionid())
+
+
+j_id = ronny.jsessionid()
+
+uuid = sponge_bob.uuid(j_id)
+
+push = Notification(uuid, j_id)
+
+push.send_daily('APN','ENGLISH','SPANISH')
