c83719401a79434dac8d04a41d0e8645a9980f28
Ronny Toribio <rtoribio@transparent.com>
2020-05-08 13:10:10 -0400

    typo

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 # from postV2 import Tester, User, Notification
 from postV2 import Tester, Notification
 
-ronny = Tester('rtoribio','Ac44life#','phone0','ronny_node4')
+ronny = Tester('username','password','acccount username ','node')
 
 push = Notification(UUID=ronny.uuid(), jsessionid=ronny.jsessionid(), device_type='FCM', k_lang='ENGLISH', l_lang='SPANISH')
 
