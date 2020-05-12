02ad8be156cc2e2930ce553fc2b66eaffb5ad64d
RTBlanco <ronnytoribio1@hotmail.com>
2020-05-11 20:56:48 -0400

    set up flask

--- example.py
+++ example.py
@@ -1,5 +1,5 @@
-# from postV2 import Tester, User, Notification
-from post import Tester, Notification
+from sticke.postV2 import Tester, Notification
+# from post import Tester, Notification
 
 ronny = Tester(username='TL username',password='TL password',account_name='user username',node_name=' node ',site=' LE,EE or GE test')
 
